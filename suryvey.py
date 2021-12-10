3609ade5febcc810c2de6340abc96d17be2b7c85
jaqx-kanuhsa <94816119+jaqx-kanuhsa@users.noreply.github.com>
2021-12-10 00:03:28 -0600

    Add files via upload

---
+++ suryvey.py
@@ -0,0 +1,12 @@
+'''
+
+This project is to survey people on a few questions
+Jaqx Kanuhsa
+
+name = input("WHat is your name?")
+
+color = input("WHat is your favorite color?")
+
+city = input("What city did you grow up in?")
+
+friend = input("Who is your best friend?")
